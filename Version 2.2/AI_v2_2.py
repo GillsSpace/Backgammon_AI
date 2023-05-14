d147d7821f21cd77a486016c520d62c82d47ab07
Westy Arch Computer 1 - Wills Erda <werda24@westminsterschool.org>
2023-05-14 19:00:54 -0400

    updated random move selection.

--- Version 2.2/AI_v2_2.py
+++ Version 2.2/AI_v2_2.py
@@ -6,62 +6,14 @@ import time
 
 #AI Types:
 def randomMove(ActiveBoard:Board, ActiveTurn:Turn):
-    algoBoard = copy.deepcopy(ActiveBoard)
-
-    #First Move
-    ActiveTurn.updatePossibleMoves(algoBoard)
+    ActiveTurn.updatePossibleMovesStandardFormat(ActiveBoard)
     possibleMoves = ActiveTurn.current_possible_moves
-    if len(possibleMoves) == 0:
-        return ()
+
+    if len(possibleMoves) > 0:
+        Move = random.choice(possibleMoves)
+        return Move
     else:
-        Move1 = random.choice(possibleMoves)
-        Move1 = (Move1[0], random.choice(Move1[1]))
-        roll = ActiveTurn.fromMoveToRoll(Move1[0],Move1[1],ActiveTurn.unused_dice,ActiveTurn.player)
-        ActiveTurn.unused_dice.remove(roll)
-        algoBoard.updateWithMove(Move1,ActiveTurn.player)
-        if algoBoard.pip[ActiveTurn.player - 1] == 0:
-            return [Move1]
-
-    ActiveTurn.updatePossibleMoves(algoBoard)
-    possibleMoves = ActiveTurn.current_possible_moves
-    if len(possibleMoves) == 0:
-        return [Move1]
-    else:
-        Move2 = random.choice(possibleMoves)
-        Move2 = (Move2[0], random.choice(Move2[1]))
-        roll = ActiveTurn.fromMoveToRoll(Move2[0],Move2[1],ActiveTurn.unused_dice,ActiveTurn.player)
-        ActiveTurn.unused_dice.remove(roll)
-        algoBoard.updateWithMove(Move2,ActiveTurn.player)
-        if algoBoard.pip[ActiveTurn.player - 1] == 0:
-            return(Move1,Move2)
-
-    if ActiveTurn.doubles_turn == True:
-        ActiveTurn.updatePossibleMoves(algoBoard)
-        possibleMoves = ActiveTurn.current_possible_moves
-        if len(possibleMoves) == 0:
-            return (Move1,Move2)
-        else:
-            Move3 = random.choice(possibleMoves)
-            Move3 = (Move3[0], random.choice(Move3[1]))
-            roll = ActiveTurn.fromMoveToRoll(Move3[0],Move3[1],ActiveTurn.unused_dice,ActiveTurn.player)
-            ActiveTurn.unused_dice.remove(roll)
-            algoBoard.updateWithMove(Move3,ActiveTurn.player)
-            if algoBoard.pip[ActiveTurn.player - 1] == 0:
-                return (Move1,Move2,Move3)
-
-        ActiveTurn.updatePossibleMoves(algoBoard)
-        possibleMoves = ActiveTurn.current_possible_moves
-        if len(possibleMoves) == 0:
-            return (Move1,Move2,Move3)
-        else:
-            Move4 = random.choice(possibleMoves)
-            Move4 = (Move4[0], random.choice(Move4[1]))
-            roll = ActiveTurn.fromMoveToRoll(Move4[0],Move4[1],ActiveTurn.unused_dice,ActiveTurn.player)
-            ActiveTurn.unused_dice.remove(roll)
-
-        return (Move1,Move2,Move3,Move4)
-    else:
-        return (Move1,Move2)
+        return
 
 def pickBestPip(ActiveBoard:Board, ActiveTurn:Turn):
     pipsList = []
@@ -90,7 +42,11 @@ def treeSearchI(ActiveBoard:Board, ActiveTurn:Turn):
 def Main(Main_Board:Board,Main_Turn:Turn,aiType):
     AI_player = aiType
     if AI_player == "Random":
+        print(f"Running Random Move Selection; Roll = {Main_Turn.roll}")
+        st = time.time()
         Moves = randomMove(Main_Board,Main_Turn)
+        et = time.time()
+        print(f"Finished Run; Elapsed Time = {et-st}; Final Move Set = {Moves}")
         return Moves
     elif AI_player == "Tree Search I":
         print(f"Running Tree Search I; Roll = {Main_Turn.roll}")
@@ -102,5 +58,9 @@ def Main(Main_Board:Board,Main_Turn:Turn,aiType):
     elif AI_player == "DRL":
         pass
     elif AI_player == "PBP":
+        print(f"Running PBP; Roll = {Main_Turn.roll}")
+        st = time.time()
         Moves = pickBestPip(Main_Board,Main_Turn)
+        et = time.time()
+        print(f"Finished Run; Elapsed Time = {et-st}; Final Move Set = {Moves}")
         return Moves
